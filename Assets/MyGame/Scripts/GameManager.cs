3d84e36d3026177958eb9a1881cbc28652615fdf
lealukic <lea.lukic@htl-salzburg.ac.at>
2021-03-23 10:09:18 +0100

    fix magical string

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
+    private const string SceneName = "MenuScene";
+    private const string SceneName1 = "MainScene";
     public static GameManager instance;
     public GameObject gameOverPanel;
     public Text scoreText;
@@ -38,12 +39,12 @@ public class GameManager : MonoBehaviour
 
     public void Restart()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene(SceneName1);
     }
 
     public void Menu()
     {
-        SceneManager.LoadScene("MenuScene");
+        SceneManager.LoadScene(SceneName);
     }
 
     public void IncrementScore()
